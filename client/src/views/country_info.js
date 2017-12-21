beaca094a9304258060a37118deb10c056ed3b19
Peter Crowther <petercrowther90@hotmail.com>
2017-12-21 10:39:03 +0000

    my changes

--- client/src/views/country_info.js
+++ client/src/views/country_info.js
@@ -3,7 +3,13 @@ const CountryInfo = function () {
 }
 
 CountryInfo.prototype.render = function (country) {
-  // TODO: Render country info
+  const ul = document.querySelector('#country-info');
+  const capital = document.createElement('li');
+  const population = document.createElement('li')
+  capital.innerText = `Capital city: ${country.capital}`
+  population.innerText = `Population:  ${country.population}`;
+  capital.appendChild(population);
+  ul.appendChild(capital);
 }
 
 CountryInfo.prototype.createListItem = function (label, content) {
